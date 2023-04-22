d2749621a1d85b7b78623cdedc8fc5fa8ca508db
DarkRec <54840416+DarkRec@users.noreply.github.com>
2023-04-22 12:46:47 +0200

    connection fastapi-mongo

--- api/app/config.py
+++ api/app/config.py
@@ -2,6 +2,7 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     CONNECTION_STRING: str
+    DB_NAME: str
     
     class Config:
         env_file = ".env"
--- api/main.py
+++ api/main.py
@@ -1,5 +1,16 @@
 from fastapi import FastAPI
 from app.config import settings
+from pymongo import MongoClient
+
 
 
 app = FastAPI()
+
+@app.on_event("startup")
+def startup_db_client():
+    app.mongodb_client = MongoClient(settings.CONNECTION_STRING)
+    app.database = app.mongodb_client[settings.DB_NAME]
+
+@app.on_event("shutdown")
+def shutdown_db_client():
+    app.mongodb_client.close()
